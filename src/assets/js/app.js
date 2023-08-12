9f608bd6a5c9d0a07e6d1584147299f2a1a4ac34
Yulia Klimchuk <89743667+yuliaklimchuk@users.noreply.github.com>
2023-08-12 14:22:52 +0300

    edits

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -19,6 +19,19 @@ new WOW().init();
   
     function toggleModal() {
       refs.modalHr.classList.toggle('is-hidden');
-      console.log('click');
     }
   })();
+
+  // Smooth animation script for page navigation
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+};
